57ca47b0cc545ea1307f9a9465a8841cb53b0f5e
masterUNG <phrombutr@gmail.com>
2019-08-07 13:52:03 +0700

    Design UI home success

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,8 +9,43 @@ class _HomeState extends State<Home> {
   // Explicit
   double myWidth = 120.0;
   double myH1 = 36.0;
+  Color myColor = Colors.orange[900];
 
   // Method
+  Widget signUpButton() {
+    return Container(
+      width: 200.0,
+      child: OutlineButton(
+        borderSide: BorderSide(color: myColor),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Text(
+          'Sign Up',
+          style: TextStyle(color: myColor),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signInButton() {
+    return Container(
+      width: 200.0,
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: myColor,
+        child: Text(
+          'Sign In',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
   Widget showLogo() {
     return Container(
       alignment: Alignment.center,
@@ -26,22 +61,32 @@ class _HomeState extends State<Home> {
     return Text(
       'Ung PiLab',
       style: TextStyle(
-        fontSize: myH1,
-        color: Colors.pink[700],
-        fontWeight: FontWeight.bold,fontFamily: 'Lacquer'
-      ),
+          fontSize: myH1,
+          color: Colors.pink[700],
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Lacquer'),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          showLogo(),
-          showAppName(),
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: [Colors.white, Colors.blue],
+            radius: 1.0,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            showLogo(),
+            showAppName(),
+            signInButton(),
+            signUpButton(),
+          ],
+        ),
       ),
     );
   }
